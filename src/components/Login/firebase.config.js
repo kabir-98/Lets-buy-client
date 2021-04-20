ea95492f0443fdbacfe6386a9612e17db536f011
Mostakimul Kabir <17-35673-3@student.aiub.edu>
2021-04-20 21:42:59 +0600

    first

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdyktaLERWbsF1FxWhGbzYZZorzcJPBFw",
+  authDomain: "buy-things-8574a.firebaseapp.com",
+  projectId: "buy-things-8574a",
+  storageBucket: "buy-things-8574a.appspot.com",
+  messagingSenderId: "951287869888",
+  appId: "1:951287869888:web:a0356d3ee3ee208c151825"
+};
